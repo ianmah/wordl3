9cbce6f0d0265f3546c15ec187167658c3c16015
unknown <ianmmah@gmail.com>
2022-03-08 00:57:33 -0800

    Implement hash func

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import CryptoJS from 'crypto-js'
+import './App.css'
+
+const GAMEID = 123456
+
+const createWord = (word) => {
+  if (word.length > 5) {
+    return
+  }
+
+  const hashes = word.split('').map((letter, i) => {
+    return CryptoJS.SHA256(letter + i + GAMEID).toString(CryptoJS.enc.Base64);
+  })
+
+  console.log(hashes)
+
+}
 
 function App() {
+  const [word, setWord] = useState('crane')
+  
+  createWord('crane')
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+        {[0,1,2,3,4].map((i) => {
+          return <div className="letter-box" key={i}>{word[i]}</div>
+        })
+      }
+      </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
