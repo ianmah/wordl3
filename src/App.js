08d763ae4f8ddd7a48ff4f55b785590fafec600d
Stanford Lin <stanfordlin@outlook.com>
2022-03-12 21:19:34 -0800

    Added ipfs sending/receiving

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Keyboard from './components/Keyboard';
 import { wordList } from './constants/data';
+import { v4 as uuid } from 'uuid';
+import { create as IpfsHttpClient } from 'ipfs-http-client'
+import { toString as uint8ArrayToString } from "uint8arrays/to-string";
 
 const App = () => {
   const [boardData, setBoardData] = useState(JSON.parse(localStorage.getItem("board-data")));
@@ -45,6 +48,82 @@ const App = () => {
       setBoardData(newBoardData);
       localStorage.setItem("board-data", JSON.stringify(newBoardData));
     }
+
+    const small_id = uuid().slice(0, 6) //This is to generate a room code
+    let ipfs
+    let topic
+    let peerId
+
+    console.log("The unique id that was generated: " + small_id)
+
+
+    async function reset() {
+      if (ipfs && topic) {
+        console.log(`Unsubscribing from topic ${topic}`)
+        await ipfs.pubsub.unsubscribe(topic)
+      }
+
+      topic = null
+      peerId = null
+      ipfs = null
+    }
+
+    async function nodeConnect(url) {
+      await reset()
+      console.log(`Connecting to ${url}`)
+      ipfs = IpfsHttpClient(url)
+      const { id, agentVersion } = await ipfs.id()
+      peerId = id
+      console.log(`<span class="green">Success!</span>`)
+      console.log(`Version ${agentVersion}`)
+      console.log(`Peer ID ${id}`)
+      await subscribe('wordl3-topic')
+      await send("hello from stan's mac")
+
+    }
+
+    async function subscribe(nextTopic) {
+      if (!nextTopic) throw new Error('Missing topic name')
+      if (!ipfs) throw new Error('Connect to a node first')
+
+      const lastTopic = topic
+
+      if (topic) {
+        topic = null
+        console.log(`Unsubscribing from topic ${lastTopic}`)
+        await ipfs.pubsub.unsubscribe(lastTopic)
+      }
+
+      console.log(`Subscribing to ${nextTopic}...`)
+
+      await ipfs.pubsub.subscribe(nextTopic, msg => {
+        const from = msg.from
+        const seqno = uint8ArrayToString(msg.seqno, 'base16')
+        if (from === peerId) return console.log(`Ignoring message ${seqno} from self`)
+        console.log(`Message ${seqno} from ${from}:`)
+        try {
+          console.log(JSON.stringify(uint8ArrayToString(msg.data), null, 2))
+        } catch (_) {
+          console.log(uint8ArrayToString(msg.data, 'base16'))
+        }
+      })
+
+      topic = nextTopic
+    }
+
+    async function send(msg) {
+      if (!msg) throw new Error('Missing message')
+      if (!topic) throw new Error('Subscribe to a topic first')
+      if (!ipfs) throw new Error('Connect to a node first')
+
+      console.log(`Sending message to ${topic}...`)
+      await ipfs.pubsub.publish(topic, msg)
+      console.log(`<span class="green">Success!</span>`)
+    }
+
+    nodeConnect('/ip4/127.0.0.1/tcp/5001')
+
+
   }, []);
 
   const handleMessage = (message) => {
