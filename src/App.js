2d707c8a058327f76203ded125c3fd46a792bfe6
Stanford Lin <stanfordlin@outlook.com>
2022-03-12 19:51:21 -0800

    added front end

--- src/App.js
+++ src/App.js
@@ -1,98 +1,184 @@
-import React, { useEffect, useState } from 'react';
-import Keyboard from './components/Keyboard';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Keyboard from './components/Keyboard';
+import { wordList } from './constants/data';
 
-const IPFS = require('ipfs')
+const App = () => {
+  const [boardData, setBoardData] = useState(JSON.parse(localStorage.getItem("board-data")));
+  const [message, setMessage] = useState(null);
+  const [error, setError] = useState(false);
+  const [charArray, setCharArray] = useState([]);
 
-function App() {
+
+  const resetBoard = () => {
+    var alphabetIndex = Math.floor(Math.random() * 26);
+    var wordIndex = Math.floor(Math.random() * wordList[String.fromCharCode(97 + alphabetIndex)].length);
+    let newBoardData = {
+      ...boardData, "solution": wordList[String.fromCharCode(97 + alphabetIndex)][wordIndex],
+      "rowIndex": 0,
+      "boardWords": [],
+      "boardRowStatus": [],
+      "presentCharArray": [],
+      "absentCharArray": [],
+      "correctCharArray": [],
+      "status": "IN_PROGRESS"
+    };
+    setBoardData(newBoardData);
+    localStorage.setItem("board-data", JSON.stringify(newBoardData));
+  }
 
 
   useEffect(() => {
-    const init = async () => {
-      
-      const ipfs = await IPFS.create({
-          repo: 'ok' + Math.random(),
-          EXPERIMENTAL: {
-            pubsub: true,
-            ipnsPubsub: true,
-          },
-          config: {
-            Addresses: {
-              Swarm: [ '/dns4/star.thedisco.zone/tcp/9090/wss/p2p-webrtc-star', '/dns6/star.thedisco.zone/tcp/9090/wss/p2p-webrtc-star' ]
-            }
-          }
-      })
-      
-      // processes a circuit-relay announce over pubsub
-      async function processAnnounce(addr) {
-        console.log(addr)
-        // get our peerid
-        const me = (await ipfs.id()).id
-        console.log(me)
-
-        // not really an announcement if it's from us
-        if (addr.from === me) {
-            return;
-        }
-
-        // if we got a keep-alive, nothing to do
-        if (addr === "keep-alive") {
-            console.log(addr);
-            return;
-        }
-
-        const peer = addr.from;
-        console.log("Peer: " + peer);
-        console.log("Me: " + me);
-        if (peer === me) { // return if the peer being announced is us
-            return;
-        }
-
-        // get a list of peers
-        const peers = await ipfs.swarm.peers();
-        for (let i in peers) {
-            // if we're already connected to the peer, don't bother doing a
-            // circuit connection
-            if (peers[i].peer === peer) {
-                return;
-            }
-        }
-        // log the address to console as we're about to attempt a connection
-        console.log(addr);
-
-        // connection almost always fails the first time, but almost always
-        // succeeds the second time, so we do this:
-        try {
-            await ipfs.swarm.connect(addr);
-        } catch(err) {
-            console.log(err);
-            await ipfs.swarm.connect(addr);
-        }
-      }
-
-			// process announcements over the relay network, and publish our own keep-alives to keep the channel alive
-      // await ipfs.pubsub.subscribe("announce-circuit", processAnnounce);
-      // setInterval(function(){ipfs.pubsub.publish("announce-circuit", "peer-alive");}, 15000);
-
-        function echo(msg) {
-          // msg = new TextDecoder().decode(msg.data);
-          console.log(msg);
-        }
-        
-        await ipfs.pubsub.subscribe("example_topic3", echo);
-
-        await ipfs.pubsub.publish("example_topic3", "Hello world!");
-
+    if (!boardData || !boardData.solution) {
+      var alphabetIndex = Math.floor(Math.random() * 26);
+      var wordIndex = Math.floor(Math.random() * wordList[String.fromCharCode(97 + alphabetIndex)].length);
+      let newBoardData = {
+        ...boardData, "solution": wordList[String.fromCharCode(97 + alphabetIndex)][wordIndex],
+        "rowIndex": 0,
+        "boardWords": [],
+        "boardRowStatus": [],
+        "presentCharArray": [],
+        "absentCharArray": [],
+        "correctCharArray": [],
+        "status": "IN_PROGRESS"
+      };
+      setBoardData(newBoardData);
+      localStorage.setItem("board-data", JSON.stringify(newBoardData));
     }
+  }, []);
 
-    init()
-  })
+  const handleMessage = (message) => {
+    setMessage(message);
+    setTimeout(() => {
+      setMessage(null);
+    }, 3000);
+  }
+
+  const handleError = () => {
+    setError(true);
+    setTimeout(() => {
+      setError(false);
+    }, 2000);
+  }
+
+  const enterBoardWord = (word) => {
+    let boardWords = boardData.boardWords;
+    let boardRowStatus = boardData.boardRowStatus;
+    let solution = boardData.solution;
+    let presentCharArray = boardData.presentCharArray;
+    let absentCharArray = boardData.absentCharArray;
+    let correctCharArray = boardData.correctCharArray;
+    let rowIndex = boardData.rowIndex;
+    let rowStatus = [];
+    let matchCount = 0;
+    let status = boardData.status;
+
+    for (var index = 0; index < word.length; index++) {
+      if (solution.charAt(index) === word.charAt(index)) {
+        matchCount++;
+        rowStatus.push("correct");
+        if (!correctCharArray.includes(word.charAt(index))) correctCharArray.push(word.charAt(index));
+        if (presentCharArray.indexOf(word.charAt(index)) !== -1) presentCharArray.splice(presentCharArray.indexOf(word.charAt(index)), 1);
+      } else if (solution.includes(word.charAt(index))) {
+        rowStatus.push("present");
+        if (!correctCharArray.includes(word.charAt(index))
+          && !presentCharArray.includes(word.charAt(index))) presentCharArray.push(word.charAt(index));
+      } else {
+        rowStatus.push("absent");
+        if (!absentCharArray.includes(word.charAt(index))) absentCharArray.push(word.charAt(index));
+      }
+    }
+    if (matchCount === 5) {
+      status = "WIN";
+      handleMessage("YOU WON")
+    }
+    else if (rowIndex + 1 === 6) {
+      status = "LOST";
+      handleMessage(boardData.solution)
+    }
+    boardRowStatus.push(rowStatus);
+    boardWords[rowIndex] = word;
+    let newBoardData = {
+      ...boardData,
+      "boardWords": boardWords,
+      "boardRowStatus": boardRowStatus,
+      "rowIndex": rowIndex + 1,
+      "status": status,
+      "presentCharArray": presentCharArray,
+      "absentCharArray": absentCharArray,
+      "correctCharArray": correctCharArray
+    };
+    setBoardData(newBoardData);
+    localStorage.setItem("board-data", JSON.stringify(newBoardData));
+  }
+
+  const enterCurrentText = (word) => {
+    let boardWords = boardData.boardWords;
+    let rowIndex = boardData.rowIndex;
+    boardWords[rowIndex] = word;
+    let newBoardData = {
+      ...boardData,
+      "boardWords": boardWords
+    };
+    setBoardData(newBoardData);
+  }
+
+  const handleKeyPress = (key) => {
+    if (boardData.rowIndex > 5 || boardData.status === "WIN") return;
+    if (key === "ENTER") {
+      if (charArray.length === 5) {
+        let word = charArray.join("").toLowerCase();
+        if (!wordList[word.charAt(0)].includes(word)) {
+          handleError();
+          handleMessage("Not in word list");
+          return;
+        }
+        enterBoardWord(word);
+        setCharArray([]);
+      } else {
+        handleMessage("Not enough letters");
+      }
+      return;
+    }
+    if (key === "⌫") {
+      charArray.splice(charArray.length - 1, 1);
+      setCharArray([...charArray]);
+    }
+    else if (charArray.length < 5) {
+      charArray.push(key);
+      setCharArray([...charArray]);
+    }
+    enterCurrentText(charArray.join("").toLowerCase());
+  }
 
   return (
-    <div className="App">
-
+    <div className='container'>
+      <div className='top'>
+        <div className='title'>WORDLE CLONE</div>
+        <button className="reset-board" onClick={resetBoard}>{"\u27f3"}</button>
+      </div>
+      {message && <div className='message'>
+        {message}
+      </div>}
+      <div className='cube'>
+        {[0, 1, 2, 3, 4, 5].map((row, rowIndex) => (
+          <div className={`cube-row ${boardData && row === boardData.rowIndex && error && "error"}`} key={rowIndex}>
+            {
+              [0, 1, 2, 3, 4].map((column, letterIndex) => (
+                <div key={letterIndex} className={`letter ${boardData && boardData.boardRowStatus[row] ? boardData.boardRowStatus[row][column] : ""}`}>
+                  {boardData && boardData.boardWords[row] && boardData.boardWords[row][column]}
+                </div>
+              ))
+            }
+          </div>
+        ))}
+      </div>
+      <div className='bottom'>
+        <Keyboard boardData={boardData}
+          handleKeyPress={handleKeyPress} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
